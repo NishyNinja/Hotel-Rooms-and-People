b72346cf8a03801cd8ba62a623d63ceef462021a
Engineering <Engineering@308-20.cls-edu.charlottelatin.org>
2022-09-07 09:51:37 -0400

    First commit

--- Hotel Rooms and People/Main.java
+++ Hotel Rooms and People/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     
     public static void main(String[] args){
-        
+        Person Bob = new Person(5.0, 18, "Bob", "Black");
+        HotelRoom room_one = new HotelRoom(1);
+        room_one.checkIn(Bob);
     }
 }
--- Hotel Rooms and People/Person.java
+++ Hotel Rooms and People/Person.java
@@ -3,7 +3,15 @@ public class Person {
     int age;
     String name;
     String hairColor;
+    boolean Hotel_Full;
 
+    Person(){
+        height = 0.0;
+        age = 0;
+        name = null;
+        hairColor = null;
+    }
+    
     Person(double h, int a, String n, String hC){
         height = h;
         age = a;
