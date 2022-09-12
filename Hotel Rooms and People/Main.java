3bfc37bd0c69f29a8e0f97b01d37802c2b477e68
NishyNinja <63650778+NishyNinja@users.noreply.github.com>
2022-09-12 09:40:15 -0400

    Delete Hotel Rooms and People directory

--- Hotel Rooms and People/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    
-    public static void main(String[] args){
-        Person Bob = new Person(5.0, 18, "Bob", "Black");
-        HotelRoom room_one = new HotelRoom(1);
-        room_one.checkIn(Bob);
-    }
-}
